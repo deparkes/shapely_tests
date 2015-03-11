497927510b22b7385d347ad9e494efafe2c31f6f
Duncan Parkes <deparkes@gmail.com>
2015-03-11 21:17:59 +0000

    Big changes

--- FilledPolygonPlot.py
+++ FilledPolygonPlot.py
@@ -8,24 +8,16 @@ A simplified version of linearring.py, one of the shapely examples
 """
 
 from matplotlib import pyplot as plt
-from shapely.geometry.polygon import LinearRing, Polygon
-
-
-
-# 1: valid ring
-
-#poly = Polygon([(0, 0), (0, 2), (1, 1), 
-#                (2, 2), (2, 0), (1, 0.8), (0, 0)])
-#x,y = poly.exterior.xy
-
-ring = LinearRing([(0, 0), (0, 2), (1, 1), (2, 2), (2, 0), (1, 0.8), (0, 0)])
-x, y = ring.xy
+from shapely.geometry.polygon import Polygon
+from descartes import PolygonPatch
 
 fig = plt.figure(1, figsize=(5,5), dpi=90)
+ring_mixed = Polygon([(0, 0), (0, 2), (1, 1), (2, 2), (2, 0), (1, 0.8), (0, 0)])
 ax = fig.add_subplot(111)
-ax.plot(x, y)
-ax.set_title('Polygon Edges')
+ring_patch = PolygonPatch(ring_mixed)
+ax.add_patch(ring_patch)
 
+ax.set_title('Filled Polygon')
 xrange = [-1, 3]
 yrange = [-1, 3]
 ax.set_xlim(*xrange)
@@ -33,3 +25,4 @@ ax.set_xticks(range(*xrange) + [xrange[-1]])
 ax.set_ylim(*yrange)
 ax.set_yticks(range(*yrange) + [yrange[-1]])
 ax.set_aspect(1)
+
